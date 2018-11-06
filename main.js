295976bbe09658a3c9a7b9cd2660ea56be1967eb
zkwong11 <44291006+zkwong11@users.noreply.github.com>
2018-11-06 22:04:59 +0800

    Fixed header bugs

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Navigation Menu
 let navPopup = document.querySelector(".nav-popup");
 
 function navToggleClick() {
@@ -11,3 +12,16 @@ function navToggleClick() {
 };
 
 document.querySelector(".nav-toggle-wrap").addEventListener("click", navToggleClick);
+
+// GSAP Animations
+TweenMax.staggerFrom('li', .3, {opacity: 0, scale: .5, y: 80, delay: 0}, .3);
+TweenMax.staggerFrom('article', .7, {opacity: 0, xPercent: 40}, .3);
+TweenMax.staggerFrom('.logo', 1, {opacity: 0, scale: .2, y: 80, delay: .5});
+
+// (function() {
+//     let h1 = document.getElementsByTagName("h1");
+//     let tl = new TimelineLite();
+
+//     tl
+//         .from(h1, .3, {x: -2000, autoAlpha: 0, ease:Power3.easeIn}, .3);
+// })();
